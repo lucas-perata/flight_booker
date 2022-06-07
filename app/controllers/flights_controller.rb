2b7fbbf4ce057737a7bd1ba0841fae3d8005865f
lucas-perata <contacto.perata@gmail.com>
2022-06-07 00:56:00 -0300

    Parametros de busqueda, arrival

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,9 @@ class FlightsController < ApplicationController
 
     def index
       @flights = Flight.all
-      @airport_options = Airport.all.map {|a| [a.code]}
+      @airport_options = Airport.all.map {|a| [a.code, a.id]}
+
+      @available_flight = Flight.where(arrival_airport_id: params[:arrival_airport_id])
     end 
  
 
