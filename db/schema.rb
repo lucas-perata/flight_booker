e3145b165a17d729e464eddc24a249b0aae28d19
lucas-perata <contacto.perata@gmail.com>
2022-06-16 22:49:21 -0300

    Model bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_07_021259) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_17_014820) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flights_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flights_id"], name: "index_bookings_on_flights_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -26,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_021259) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookings", "flights", column: "flights_id"
 end
